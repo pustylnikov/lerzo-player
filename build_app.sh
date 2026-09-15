226dc9af322009dbebf5c98d894630032aa2a6c3
yurii <yura.pustylnikov@gmail.com>
2026-09-15 14:22:09 +0300

    Bump version to 1.2.0

--- build_app.sh
+++ build_app.sh
@@ -67,7 +67,7 @@ cat << 'EOF' > "$CONTENTS/Info.plist"
     <key>CFBundlePackageType</key>
     <string>APPL</string>
     <key>CFBundleShortVersionString</key>
-    <string>1.1.1</string>
+    <string>1.2.0</string>
     <key>CFBundleVersion</key>
     <string>1</string>
     <key>LSMinimumSystemVersion</key>
